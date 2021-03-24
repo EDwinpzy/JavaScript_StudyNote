fe5de7895f76cc4b0b32c254255329b28123cfd6
EDwinpzy <630548043@qq.com>
2021-03-24 20:25:39 +0800

    3.24

--- 原型链继承问题.js
+++ 原型链继承问题.js
@@ -1,3 +1,5 @@
+//可参考 https://blog.csdn.net/weixin_36465540/article/details/89885292?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522161658676616780271586175%2522%252C%2522scm%2522%253A%252220140713.130102334.pc%255Fall.%2522%257D&request_id=161658676616780271586175&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~all~first_rank_v2~rank_v29-3-89885292.first_rank_v2_pc_rank_v29_10&utm_term=js%E4%B8%AD%E9%87%8D%E5%86%99%E8%B6%85%E7%B1%BB
+
 //定义父类构造函数
 function SuperType() {
     this.property = true;
@@ -24,5 +26,5 @@ SubType.prototype.getSuperValue = function() {
 var instance = new SubType();
 var instance_1 = new SuperType();
 //重写的getSuperValue方法是在SubType.prototype里面，创建SubType实例从原型链下到上检索到的是重写后的getSuperValue方法，而创建的SuperType实例时重写前的
-alert(instance.getSuperValue);  //false
-alert(instance_1.getSuperValue);    //true
+console.log(instance.getSuperValue);  //false
+console.log(instance_1.getSuperValue);    //true
